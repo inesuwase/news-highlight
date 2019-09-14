a17d1a6df14de1f02dc633be9ff8f43f345e44af
ines uwase <uwaseines10@gmail.com>
2019-09-14 10:45:30 +0200

    defined route decorators

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,8 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from . import main
+from ..requests import get_movies,get_movie,search_movie
+from .forms import ReviewForm
+from ..models import Review
 
 # Views
 @main.route('/')
